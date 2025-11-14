fd04e2da7fbe57c30ee9b119439b6eaefe7b423e
yean <yean@gmail.com>
2025-11-14 12:26:42 +0700

    update!!

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 // ================================
 // ✅ Configuration Variables
 // ================================
-const MODEL_URL = "./models"; // path to face-api.js models
+const MODEL_URL = "https://cingcing12.github.io/System_Dashboard/models/"; // path to face-api.js models
 let modelsLoaded = false;
 let streamRef = null;
 let currentFacing = "user"; // "user" = front, "environment" = back
