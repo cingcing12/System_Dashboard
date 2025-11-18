f2669410eb828cb10dd05902b17b1703af7bf5ab
cingcing12 <cing16339@gmail.com>
2025-11-18 09:19:49 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const GITHUB_BRANCH = process.env.GITHUB_BRANCH || "main";
 // ---------------------------
 // SheetDB Config
 // ---------------------------
-const SHEETDB_BASE_URL = "https://sheetdb.io/api/v1/t3vhnptj22b4g";
+const SHEETDB_BASE_URL = "https://sheetdb.io/api/v1/oz77mi92j3l73";
 const SHEET_USERS = "Users";
 function sheetUrl(sheetName) {
   return `${SHEETDB_BASE_URL}?sheet=${sheetName}`;
