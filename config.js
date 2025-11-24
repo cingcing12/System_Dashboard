66aeda00ce53045d71ce237f61f9e00aa1263209
cingcing12 <cing16339@gmail.com>
2025-11-24 08:56:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ✅ Your unique SheetDB API base URL
-const SHEETDB_BASE_URL = "https://sheetdb.io/api/v1/qik1anddidakk";
+const SHEETDB_BASE_URL = "https://sheetdb.io/api/v1/15edyx91e1lkq";
 
 // Tabs (Sheets)
 const SHEET_EXPENSES = "Expenses";
